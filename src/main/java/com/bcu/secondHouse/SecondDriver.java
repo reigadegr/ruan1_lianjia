a25a6156fcadaccb4190c3e359389f5b8492eb57
reigadegr <2722688642@qq.com>
2024-03-21 09:56:20 +0800

    添加main方法

--- src/main/java/com/bcu/secondHouse/SecondDriver.java
+++ src/main/java/com/bcu/secondHouse/SecondDriver.java
@@ -18,11 +18,33 @@ public class SecondDriver extends Configured implements Tool {
         job.setJarByClass(SecondDriver.class);
         job.setInputFormatClass(TextInputFormat.class);
         //指定地址
-        TextInputFormat.addInputPath(job,new Path("hdfs://node-1:9000/secondhouse"));
+        TextInputFormat.addInputPath(job, new Path("hdfs://node-1:9000/secondhouse"));
         //配置map
+        //告知mapper类是哪个
+        job.setMapperClass(SecondMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+        //3,shuffer
 
-        return 0;
+
+        //4，配置reduce
+        //告知reduce类是哪个
+        job.setReducerClass(SecondReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+        //5，配置结果输出
+        job.setOutputFormatClass(TextOutputFormat.class);
+        //指定输出结果的地址
+        TextOutputFormat.setOutputPath(job, new Path("hdfs://node-1:9000/secondhouse_out"));
+
+        //提交：submit
+        boolean b = job.waitForCompletion(true);
+        return b ? 0 : 1;
     }
 
+    public static void main(String[] args) throws Exception {
+        int status = ToolRunner.run(new Configuration(), new SecondDriver(), args);
+        System.exit(status);
+    }
 
 }
